d82d50290c87c1cbeac722972d1ed821dc5e395e
muhammadJamshed <ajmehsud12@gmail.com>
2024-05-06 17:47:55 +0500

    learning

---
+++ my-app/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Typo from '@/components/Typo'
+export default function Home() {
+  return (
+    <main>
+    <Typo/>
+    </main>
+  );
+}
